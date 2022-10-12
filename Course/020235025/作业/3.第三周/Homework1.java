0da44402f19b5d5001fc42156fe64d74401b0208
summeriver <83163060+FakeGod13@users.noreply.github.com>
2022-10-12 19:56:17 +0800

    java作业：删除源代码文件

--- Course/020235025/作业/3.第三周/Homework1.java
+++
@@ -1,9 +0,0 @@
-public class Homework1 {
-    public static void main(String args[]){
-        System.out.println(func(30));
-    }
-    public static int func(int n) {
-        if(n==1||n==2) return 1;
-        else return func(n-1)+func(n-2);
-    }
-}
